97c90db55a281a0b1f475c4e99bd1bbd9150a168
CHENShuang1994 <979524237@qq.com>
2016-05-08 18:46:00 +0800

    fix recv error lalalalalalalalalallalalal

--- proxy.py
+++ proxy.py
@@ -5,8 +5,34 @@ Created on Sat May 07 16:05:17 2016
 @author: CHEN Shuang
 """
 import re
+import time
 from socket import *
 #class define 
+def recv_timeout(from_socket, to_socket,timeout=2):
+    from_socket.setblocking(0)
+    #total_data=[];
+    data='';
+    begin=time.time()
+    recvGzero = False
+    while 1:
+    #if you got some data, then break after wait sec
+        if recvGzero and time.time()-begin>timeout:
+            break
+    #if you got no data at all, wait a little longer
+        elif time.time()-begin>timeout*2:
+            break
+        try:
+            data=from_socket.recv(8192)
+            if data:
+                recvGzero = True
+                to_socket.sendall(data)
+                #total_data.append(data)
+                begin=time.time()
+            else:
+                time.sleep(0.1)
+        except:
+            pass
+    return True
 class httpHeader(object):
     def __init__(self, method=None, url=None, cookie=None, host=None):
         self.method = method
@@ -35,16 +61,17 @@ def parseHttpHeader(request, header):
         header.cookie = result.group(1)
     else:
         print "Cookie in request is null"
-        
+responsesFile = open("response.txt", 'w')    
 #parameter definition
 serverPort = 8080
 httpPort = 80
-recvSize = 65507
+recvSize = 8960
+timeout = 10
 #http header
 Header = httpHeader()
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(('',serverPort))
-serverSocket.listen(100)
+serverSocket.listen(50)
 print "the proxy server is listening"
 while True:
     #recv from client
@@ -52,26 +79,53 @@ while True:
     httpRequest = connectionSocket.recv(recvSize)
     #proxy processing
     parseHttpHeader(httpRequest, Header)
-    print httpRequest
+    httpResponse = ''
     try:
         #print repr(Header.host)
-        proxyClientSocket = socket(AF_INET, SOCK_STREAM)
         #print getaddrinfo(Header.host, httpPort)[0][4]
-        proxyClientSocket.connect(getaddrinfo(Header.host, httpPort)[0][4])
+        proxyClientSocket = socket(AF_INET, SOCK_STREAM)
+        (soc_family, _, _, _, address) = getaddrinfo(Header.host, httpPort)[0]     
+        print address
+        proxyClientSocket.connect(address)        
         #proxyClientSocket.connect((Header.host, httpPort))
         print "Proxy server connect host: %s succeed" % Header.host
-        proxyClientSocket.send(httpRequest)
-        httpResponse = proxyClientSocket.recv(recvSize)
-        print httpRequest
+        
+        #print httpRequest
+        #print httpRequest
+        proxyClientSocket.sendall(httpRequest)
+        recv_timeout(proxyClientSocket, connectionSocket)
+        proxyClientSocket.close()
+        connectionSocket.close()
+#        ret = proxyClientSocket.recv(recvSize)
+#        print "Http response", ret
+#        connectionSocket.send(ret)
+#        proxyClientSocket.close()
+#        connectionSocket.close()
+#        begin = time.time()
+#        while (True):
+#            print "beginrecv"
+#            ret = proxyClientSocket.recv() 
+#            print "endrecv"            
+#            if (ret):
+#                print '----'
+#                print ret
+#                print '---'
+#                connectionSocket.send(ret)
+#            else:
+#                print "---------------------------\nhttpResponse send to %s succeed" % Header.host
+#                proxyClientSocket.close()
+#                connectionSocket.close()   
+#                break
+        responsesFile.close()
+        #print httpResponse
         #print Header    
         #send back to client
-        connectionSocket.send(httpResponse)
+#        print httpResponse
+#        connectionSocket.sendall(httpResponse
     except:
+        #assert  (Header.host!='today.hit.edu.cn')
         print "Connect %s failed" % Header.host
         proxyClientSocket.close()
         connectionSocket.close()
         continue
-    else:
-        proxyClientSocket.close()
-        connectionSocket.close()        
-#    
+responsesFile.close()
